f33a90e8f9604ef206b73379ac597f101fe72b55
SilviuDinu <silviu_dinu@yahoo.com>
2021-06-05 12:21:50 +0300

    add city endpoint and mock endpoint

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const path = require("path");
-var http = require('http');
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
-const csp = require("helmet-csp");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const csp = require('helmet-csp');
 const middleware = require('./policies/middleware');
-require("dotenv").config();
+const current_weather = require('./mocks/current-weather');
+const { default: axios } = require('axios');
+require('dotenv').config();
 
 const API_KEY = process.env.API_KEY;
 const BASE_URL = process.env.WEATHER_API_BASE_URL;
@@ -14,31 +15,37 @@ const BASE_URL = process.env.WEATHER_API_BASE_URL;
 const app = express();
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("Running on port " + port);
+  console.log('Running on port ' + port);
 });
-app.enable("trust proxy");
+app.enable('trust proxy');
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy(middleware));
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
-
 // API
-app.get("/", async (req, res) => {
-  res.send('Hello')
+app.get('/', async (req, res) => {
+  res.send('Hello');
 });
 
-app.get('/api/current/', (req, res) => {
-  const { cityName } = req.query;
-  try {
-    const options = new URL(`${BASE_URL}?q=${cityName}&units=metric&appid=${API_KEY}`);
-    http.request(options, (res) => {
-      console.log(res);
+app.get('/api/current/city', (req, res) => {
+  const { cityName, units = 'metric' } = req.query;
+  axios
+    .get(`https://${BASE_URL}?q=${cityName}&units=${units}&appid=${API_KEY}`)
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      res.status(400).send({ error });
     });
-  }
-  catch {
-    console.error('GET /api/current/:cityName went wrong')
-  }
 });
 
+app.get('/mockapi/current/all', (req, res) => {
+  try {
+    res.json(current_weather);
+  } 
+  catch {
+    res.status(400).send({ error: 'Something went wrong with the mock' });
+  }
+});
