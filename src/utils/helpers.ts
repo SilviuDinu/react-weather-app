159c502af7592d86b7051d39dff416acc3fedbd2
SilviuDinu <silviu_dinu@yahoo.com>
2021-06-09 09:24:19 +0300

    update helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,5 @@
 export const buildSearchParams = (params: any) => {
-    return Object.keys(params)
-        .map((key: any, idx: number) =>
-            idx === 0 ? `?${key}=${params[key]}` : `&${key}=${params[key]}`
-        ).join('');
+    return encodeURI(`?${Object.keys(params)
+        .map((key: string) => `${key}=${params[key]}`)
+        .join('&')}`);
 }
