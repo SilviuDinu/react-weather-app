bd50fe5f399c87cabcbdf9e319e9050c535cdde5
SilviuDinu <silviu_dinu@yahoo.com>
2021-06-28 16:01:55 +0300

    add audio player to listen to audio books

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,30 @@
 import { ABOUT_MESSAGES } from '@enums/about.enum';
+import { useState } from 'react';
 
 export default function About(props: any) {
+  const [source, setSource] = useState('');
+
+  const loadAudio = (e: any, source?: any[]) => {
+    const files = e ? e.target.files : source;
+    console.log(files[0])
+    var file = URL.createObjectURL(files[0]);
+    setSource(file);
+  }
+
   return (
     <div className="about-page">
       <h1 className="about-page-title">{ABOUT_MESSAGES.TITLE}</h1>
       <div className="about-page-content">{ABOUT_MESSAGES.BODY}</div>
+
+      <div className="audio-wrapper">
+        <div className="audio-file">
+          <input id="audio_file" type="file" onChange={(e) => loadAudio(e)} accept="audio/*" />
+        </div>
+        <div className="audio">
+          <audio src={source} controls></audio>
+        </div>
+      </div>
+
     </div>
   );
 }
